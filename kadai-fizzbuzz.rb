ad898ca471f2a102a74067425875896462407f1f
aoi.yasunaga <aoi.y0126n@gmail.com>
2021-06-01 09:03:52 +0000

    second　message

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,15 @@
 def fizzbuzz(num)
-   if num % 3 == 0 && num % 5 == 0 
-      puts "FizzBuzz"
+   if num % 15 == 0 
+     return "FizzBuzz"
    elsif num % 3 == 0 
-      puts "Fizz"
+     return "Fizz"
    elsif num % 5 == 0
-      puts "Buzz"
+     return "Buzz"
    else 
-      puts num
+     return num
    end
 end
 num_max = 100
 (1..num_max).each do |num|
-   fizzbuzz(num)
+   puts fizzbuzz(num)
 end
-
